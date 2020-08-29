41abf4462d09c5efc6123b79ae451a1e9e0fc6f9
Kniffen <knifftech@gmail.com>
2020-08-29 17:03:04 +0200

    Made addon context-aware
    Updated the addon to use the N-API

--- scsSDKTelemetry.cc
+++ scsSDKTelemetry.cc
@@ -1,53 +1,44 @@
-#include <node.h>
+#include <node_api.h>
 #include <windows.h>
 
-namespace scsSDKTelemetry {
-  using v8::FunctionCallbackInfo;
-  using v8::Isolate;
-  using v8::Local;
-  using v8::Object;
-  using v8::Value;
-  using v8::ArrayBuffer;
-  using v8::Exception;
-  using v8::String;
-  using v8::NewStringType;
+HANDLE hMapFileSCSTelemetry;
+HANDLE hMapFileSimTelemetrySCS;
+HANDLE hMapFileSimTelemetryETS2;
+LPVOID pBuf = NULL;
 
-  HANDLE hMapFileSCSTelemetry;
-  HANDLE hMapFileSimTelemetrySCS;
-  HANDLE hMapFileSimTelemetryETS2;
-  LPVOID pBuf = NULL;
+napi_value GetArrayBuffer(napi_env env, napi_callback_info info) {
+  napi_status status;
+  napi_value buffer;
 
-  void GetArrayBuffer(const FunctionCallbackInfo<Value>& args) {
-    hMapFileSCSTelemetry     = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, "Local\\SCSTelemetry");
-    hMapFileSimTelemetrySCS  = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, "Local\\SimTelemetrySCS");
-    hMapFileSimTelemetryETS2 = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, "Local\\SimTelemetryETS2");
-    
-    if (hMapFileSCSTelemetry) {
-      pBuf = (LPVOID) MapViewOfFile(hMapFileSCSTelemetry, FILE_MAP_ALL_ACCESS, 0, 0, 32*1024);
-    
-    } else if (hMapFileSimTelemetrySCS) {
-      pBuf = (LPVOID) MapViewOfFile(hMapFileSimTelemetrySCS, FILE_MAP_ALL_ACCESS, 0, 0, 16*1024);
-    
-    } else if (hMapFileSimTelemetryETS2) {
-      pBuf = (LPVOID) MapViewOfFile(hMapFileSimTelemetryETS2, FILE_MAP_ALL_ACCESS, 0, 0, 16*1024);
-    
-    } else {
-      Isolate* isolate = args.GetIsolate();
-      isolate->ThrowException(Exception::TypeError(
-        String::NewFromUtf8(isolate, "Unable to get array buffer", NewStringType::kNormal).ToLocalChecked())
-      );
+  hMapFileSCSTelemetry     = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, "Local\\SCSTelemetry");
+  hMapFileSimTelemetrySCS  = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, "Local\\SimTelemetrySCS");
+  hMapFileSimTelemetryETS2 = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, "Local\\SimTelemetryETS2");
 
-      return;
-    }
-  
-    Local<ArrayBuffer> buffer = ArrayBuffer::New(Isolate::GetCurrent(), pBuf, 16*1024);
-
-    args.GetReturnValue().Set(buffer);
+  if (hMapFileSCSTelemetry) {
+    pBuf = MapViewOfFile(hMapFileSCSTelemetry, FILE_MAP_ALL_ACCESS, 0, 0, 32*1024);  
+  } else if (hMapFileSimTelemetrySCS) {
+    pBuf = MapViewOfFile(hMapFileSimTelemetrySCS, FILE_MAP_ALL_ACCESS, 0, 0, 16*1024);
+  } else if (hMapFileSimTelemetryETS2) {
+    pBuf = MapViewOfFile(hMapFileSimTelemetryETS2, FILE_MAP_ALL_ACCESS, 0, 0, 16*1024);
+  } else {
+    napi_throw_error(env, "200", "Unable to get array buffer");
   }
 
-  void init(Local<Object> exports) {
-    NODE_SET_METHOD(exports, "getArrayBuffer", GetArrayBuffer);
-  }
+  status = napi_create_external_arraybuffer(env, pBuf, 16*1024, NULL, NULL, &buffer);
 
-  NODE_MODULE(addon, init)
+  return buffer;
 }
+
+#define DECLARE_NAPI_METHOD(name, func)                                        \
+  { name, 0, func, 0, 0, 0, napi_default, 0 }
+
+napi_value Init(napi_env env, napi_value exports) {
+  napi_status status;
+  
+  napi_property_descriptor desc = DECLARE_NAPI_METHOD("getArrayBuffer", GetArrayBuffer);
+  status = napi_define_properties(env, exports, 1, &desc);
+  
+  return exports;
+}
+
+NAPI_MODULE(NODE_GYP_MODULE_NAME, Init)
