8dff283223fb869f7ef4b10f51138ff15256fd12
Kniffen <knifftech@gmail.com>
2021-04-01 16:02:41 +0200

    Update game.js
    Improved data for fine, tollgate, ferry, train and refuel-paid game events

--- src/lib/eventEmitters/game.js
+++ src/lib/eventEmitters/game.js
@@ -13,24 +13,64 @@ export default function eventEmittersGame(telemetry, data) {
  
 
   // Fine
-  if (data[0]?.events?.fine.active && !data[1]?.events?.fine.active)
-    telemetry.game.emit("fine", cloneDeep(data[0].events.fine))
+  if ( data[0]?.events?.fine.active && !data[1]?.events?.fine.active ) {
+    const fineEventData = cloneDeep( data[0].events.fine )
+
+    const fineData = {
+      offence: fineEventData.offence,
+      amount:  fineEventData.amount,
+    }
+
+    telemetry.game.emit( "fine", fineData )
+  }
 
   // Tollgate
-  if (data[0]?.events?.tollgate.active && !data[1]?.events?.tollgate.active)
-    telemetry.game.emit("tollgate", cloneDeep(data[0].events.tollgate))
+  if ( data[0]?.events?.tollgate.active && !data[1]?.events?.tollgate.active ) {
+    const tollgateEventData = cloneDeep( data[0].events.tollgate )
+
+    const tollgateData = {
+      amount: tollgateEventData.amount,
+    }
+
+    telemetry.game.emit( "tollgate", tollgateData )
+  }
 
   // Ferry
-  if (data[0]?.events?.ferry.active && !data[1]?.events?.ferry.active)
-    telemetry.game.emit("ferry", cloneDeep(data[0].events.ferry))
+  if ( data[0]?.events?.ferry.active && !data[1]?.events?.ferry.active ) {
+    const ferryEventData = cloneDeep( data[0].events.ferry )
+
+    const ferryData = {
+      source:      ferryEventData.source,
+      destination: ferryEventData.destination,
+      target:      ferryEventData.destination, // Deprecated, to be removed at some point
+      amount:      ferryEventData.amount,
+    }
+
+    telemetry.game.emit( "ferry", ferryData )
+  }
 
   // Train
-  if (data[0]?.events?.train.active && !data[1]?.events?.train.active)
-    telemetry.game.emit("train", cloneDeep(data[0].events.train))
+  if ( data[0]?.events?.train.active && !data[1]?.events?.train.active ) {
+    const trainEventData = cloneDeep(data[0].events.train)
+
+    const trainData = {
+      source:      trainEventData.source,
+      destination: trainEventData.destination,
+      target:      trainEventData.destination, // Deprecated, to be removed at some point
+      amount:      trainEventData.amount,
+    }
+
+    telemetry.game.emit( "train", trainData )
+  }
 
   // Refuel paid
-  if (data[0]?.events?.refuelPaid.active && !data[1]?.events?.refuelPaid.active) {
-    telemetry.game.emit("refuel-payed")
-    telemetry.game.emit("refuel-paid",  data[0].events.refuelPaid)
+  if ( data[0]?.events?.refuelPaid.active && !data[1]?.events?.refuelPaid.active ) {
+    const refuelPaidEventData = cloneDeep( data[0].events.refuelPaid )
+
+    const refuelPaidData = {
+      amount: refuelPaidEventData.amount
+    }
+
+    telemetry.game.emit( "refuel-paid", refuelPaidData )
   }
 }
