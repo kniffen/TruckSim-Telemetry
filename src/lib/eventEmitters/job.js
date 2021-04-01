908ebef9c5a9d588b65dfd0c9bb8a1fcbc105d1a
Kniffen <knifftech@gmail.com>
2021-04-01 16:03:00 +0200

    Update job.js
    Improved data for started, cancelled and delivered job events

--- src/lib/eventEmitters/job.js
+++ src/lib/eventEmitters/job.js
@@ -7,16 +7,70 @@ export default function eventEmittersJob(telemetry, data) {
     telemetry.job.emit("finished")
 
   // Job started
-  if (data[0]?.events?.job.started.active && !data[1]?.events?.job.started.active)
-      telemetry.job.emit("started", cloneDeep(data[0].events.job.started))
+  if ( data[0]?.events?.job.started.active && !data[1]?.events?.job.started.active ) {
+    const jobStartedEventData = cloneDeep( data[0].events.job.started )
+    const jobData             = cloneDeep( data[0].job )
 
+    const jobStartedData = {
+      autoLoaded:      jobStartedEventData.autoLoaded,
+      deliveryTime:    jobData.deliveryTime,
+      plannedDistance: jobData.plannedDistance,
+      cargo:           jobData.cargo,
+      isSpecial:       jobData.isSpecial,
+      source:          jobData.source,
+      destination:     jobData.destination,
+      market:          jobData.market,
+      income:          jobData.income,
+    }
+
+    telemetry.job.emit( "started", jobStartedData )
+  }
 
   // Job cancelled
-  if (data[0]?.events?.job.cancelled.active && !data[1]?.events?.job.cancelled.active)
-    telemetry.job.emit("cancelled", cloneDeep(data[0].events.job.cancelled))
+  if ( data[0]?.events?.job.cancelled.active && !data[1]?.events?.job.cancelled.active ) {
+    const jobCancelledEventData = cloneDeep( data[0].events.job.cancelled )
+    const jobData               = cloneDeep( data[1].job )
+
+    const jobCancelledData = {
+      penalty:         jobCancelledEventData.penalty,
+      startingTime:    jobCancelledEventData.startingTime,
+      finishingTime:   jobCancelledEventData.finishingTime,
+      deliveryTime:    jobData.deliveryTime,
+      plannedDistance: jobData.plannedDistance,
+      cargo:           jobData.cargo,
+      isSpecial:       jobData.isSpecial,
+      source:          jobData.source,
+      destination:     jobData.destination,
+      market:          jobData.market,
+      income:          0,
+    }
+    
+    telemetry.job.emit( "cancelled", jobCancelledData )
+  }
 
   // Job delivered
-  if (data[0]?.events?.job.delivered.active && !data[1]?.events?.job.delivered.active)
-      telemetry.job.emit("delivered", cloneDeep(data[0].events.job.delivered))
+  if ( data[0]?.events?.job.delivered.active && !data[1]?.events?.job.delivered.active ) {
+    const jobDeliveredEventData = cloneDeep( data[0].events.job.delivered )
+    const jobData               = cloneDeep( data[1].job )
+
+    const jobDeliveredData = {
+      deliveryTime:    jobDeliveredEventData.deliveryTime,
+      startingTime:    jobDeliveredEventData.startingTime,
+      finishingTime:   jobDeliveredEventData.finishingTime,
+      earnedXP:        jobDeliveredEventData.earnedXP,
+      cargoDamage:     jobDeliveredEventData.cargoDamage,
+      distance:        jobDeliveredEventData.distance,
+      autoParked:      jobDeliveredEventData.autoParked,
+      revenue:         jobDeliveredEventData.revenue,
+      plannedDistance: jobData.plannedDistance,
+      cargo:           jobData.cargo,
+      isSpecial:       jobData.isSpecial,
+      source:          jobData.source,
+      destination:     jobData.destination,
+      market:          jobData.market,
+    }
+
+    telemetry.job.emit( "delivered", jobDeliveredData )
+  }
 
 }
