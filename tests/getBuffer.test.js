cb6d79eba4a9cf14b220a3b01bdf97567f204a19
Kniffen <knifftech@gmail.com>
2021-05-22 10:52:52 +0200

    Improved error handling

--- tests/getBuffer.test.js
+++ tests/getBuffer.test.js
@@ -5,28 +5,40 @@ const getBuffer = require('../lib/getBuffer')
 const scsSDKTelemetry = require('../build/Release/scsSDKTelemetry')
 
 describe('getBuffer()', function() {
+  let buffers
+
   const opts = {
     mmfName: 'foobar'
   }
 
-  const sandbox = sinon.createSandbox()
-
   before(function() {
+    sinon.spy(getBuffer, 'default')
+
+    buffer = getBuffer(opts)
+    
     // TODO: Figure out to make the following work
     // Current error: TypeError: Cannot redefine property: getArrayBuffer
-    
-    //sandbox.stub(scsSDKTelemetry, 'getArrayBuffer')
+    // sinon.stub(scsSDKTelemetry, 'getArrayBuffer')
+  })
+
+  after(function() {
+    sinon.restore()
   })
 
   
   it('Should return a buffer')
 
-  it('Should return a buffer with a specified mapped memory file name')
+  it('Should return a buffer with a specified mapped memory file name', function() {
+    assert.deepStrictEqual(
+      getBuffer.default.args,
+      [
+        [{mmfName: 'foobar'}],
+      ]
+   )
+  })
 
-  it('Should return undefined if it fails', function() {
-    const buffer = getBuffer(opts)
-    
-    assert(buffer === undefined)
+  it('Should return "null" if it fails', function() {
+    assert.strictEqual(buffer, null)
   })
 
 })
