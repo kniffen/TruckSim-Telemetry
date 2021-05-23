88b46c1ebb72873f5d71b8423e497bbfd63d6c66
Kniffen <knifftech@gmail.com>
2021-05-23 14:46:49 +0200

    Update getBuffer.test.js

--- tests/functions/getBuffer.test.js
+++ tests/functions/getBuffer.test.js
@@ -14,19 +14,22 @@ describe('getBuffer()', function() {
   before(function() {
     sinon.spy(functions, 'getBuffer')
 
+    sinon
+      .stub(utils, 'getArrayBuffer')
+      .callsFake(() => new ArrayBuffer(8))
+
     buffer = functions.getBuffer(opts)
-    
-    // TODO: Figure out to make the following work
-    // Current error: TypeError: Cannot redefine property: getArrayBuffer
-    // sinon.stub(scsSDKTelemetry, 'getArrayBuffer')
   })
 
   after(function() {
     sinon.restore()
   })
-
   
-  it('Should return a buffer')
+  it('Should return a buffer', function() {
+    assert.ok(Buffer.isBuffer(buffer))
+
+    assert.deepStrictEqual(buffer.toJSON().data, [0,0,0,0,0,0,0,0])
+  })
 
   it('Should return a buffer with a specified mapped memory file name', function() {
     assert.deepStrictEqual(
@@ -34,10 +37,16 @@ describe('getBuffer()', function() {
       [
         [{mmfName: 'foobar'}],
       ]
-   )
+    )
+
+    assert.deepStrictEqual(utils.getArrayBuffer.args, [['foobar']])
   })
 
   it('Should return "null" if it fails', function() {
+    utils.getArrayBuffer.restore()
+
+    buffer = functions.getBuffer(opts)
+
     assert.strictEqual(buffer, null)
   })
 
