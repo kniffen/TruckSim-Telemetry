069faba98fc57a17045691e768a5d71b7a8964c7
Kniffen <knifftech@gmail.com>
2021-05-26 20:15:49 +0200

    Fixed job data for events

--- tests/eventEmitters/job.test.js
+++ tests/eventEmitters/job.test.js
@@ -8,7 +8,7 @@ const functions = require('../../lib/functions')
 
 describe('eventEmitters/job()', function() {
 
-  let clock    = null
+  let clock = null
 
   let testData = {
     events: {
@@ -23,19 +23,19 @@ describe('eventEmitters/job()', function() {
         cancelled: {
           active:        false,
           finishingTime: 1000,
-          penalty:       1001,
-          startingTime:  1002,
+          penalty:       2000,
+          startingTime:  3000,
         },
         delivered: {
           active:          false,
           autoParked:      true,
-          cargoDamage:     2000,
-          deliveryTime:    2001,
-          distance:        2002,
-          earnedXP:        2003,
-          finishingTime:   2004,
-          revenue:         2005,
-          startingTime:    2006,
+          cargoDamage:     4000,
+          deliveryTime:    5000,
+          distance:        6000,
+          earnedXP:        7000,
+          finishingTime:   8000,
+          revenue:         9000,
+          startingTime:    1100,
         },
       }
     },
@@ -43,11 +43,11 @@ describe('eventEmitters/job()', function() {
       isSpecial:       true,
       cargo:           'foo',
       market:          'bar',
-      deliveryTime:    3000,
-      income:          3001,
-      plannedDistance: 3002,
-      destination:     {foo: 'bar'},
-      source:          {bar: 'baz'},
+      deliveryTime:    2100,
+      income:          3100,
+      plannedDistance: 4100,
+      destination:     {foo: 'baz'},
+      source:          {bar: 'qux'},
     },
     game:     {},
     trailers: [],
@@ -62,7 +62,23 @@ describe('eventEmitters/job()', function() {
 
     sinon
       .stub(functions, 'getData')
-      .callsFake(() => cloneDeep(testData))
+      .callsFake(() => {
+        const data = cloneDeep(testData)
+
+        testData.job.isSpecial = !testData.job.isSpecial
+
+        testData.job.cargo  = testData.job.cargo  == 'foo' ? 'oof' : 'foo' 
+        testData.job.market = testData.job.market == 'bar' ? 'rab' : 'bar' 
+
+        testData.job.deliveryTime++
+        testData.job.income++
+        testData.job.plannedDistance++
+        
+        testData.job.destination.foo = testData.job.destination.foo == 'baz' ? 'zab' : 'baz'
+        testData.job.source.bar      = testData.job.source.bar      == 'qux' ? 'xuq' : 'qux'
+
+        return data
+      })
 
     telemetry.watch()
 
@@ -108,74 +124,120 @@ describe('eventEmitters/job()', function() {
   })
   
   it('Should emit "started" events', function() {
-    const expectedData = {
-      autoLoaded:      true,
-      isSpecial:       true,
-      cargo:           'foo',
-      market:          'bar',
-      deliveryTime:    3000,
-      income:          3001,
-      plannedDistance: 3002,
-      destination:     {foo: 'bar'},
-      source:          {bar: 'baz'},
-    }
+    const expectedData = [
+      {
+        autoLoaded:      true,
+        isSpecial:       true,
+        cargo:           'foo',
+        market:          'bar',
+        deliveryTime:    2102,
+        income:          3102,
+        plannedDistance: 4102,
+        destination:     {foo: 'baz'},
+        source:          {bar: 'qux'},
+      },
+      {
+        autoLoaded:      true,
+        isSpecial:       true,
+        cargo:           'foo',
+        market:          'bar',
+        deliveryTime:    2104,
+        income:          3104,
+        plannedDistance: 4104,
+        destination:     {foo: 'baz'},
+        source:          {bar: 'qux'},
+      }
+    ]
 
     assert.deepStrictEqual(
       telemetry.job.emit.args.filter(event => event[0] === 'started'),
       [
-        ['started', expectedData],
-        ['started', expectedData],
+        ['started', expectedData[0]],
+        ['started', expectedData[1]],
       ]
     )
   })
   
   it('Should emit "cancelled" events', function() {
-    const expectedData = {
-      isSpecial:       true,
-      cargo:           'foo',
-      market:          'bar',
-      deliveryTime:    3000,
-      finishingTime:   1000,
-      income:          0,
-      penalty:         1001,
-      plannedDistance: 3002,
-      startingTime:    1002,
-      destination:     {foo: 'bar'},
-      source:          {bar: 'baz'},
-    }
+    const expectedData = [
+      {
+        isSpecial:       false,
+        cargo:           'oof',
+        market:          'rab',
+        deliveryTime:    2101,
+        finishingTime:   1000,
+        income:          0,
+        penalty:         2000,
+        plannedDistance: 4101,
+        startingTime:    3000,
+        destination:     {foo: 'zab'},
+        source:          {bar: 'xuq'},
+      },
+      {
+        isSpecial:       false,
+        cargo:           'oof',
+        market:          'rab',
+        deliveryTime:    2103,
+        finishingTime:   1000,
+        income:          0,
+        penalty:         2000,
+        plannedDistance: 4103,
+        startingTime:    3000,
+        destination:     {foo: 'zab'},
+        source:          {bar: 'xuq'},
+      }
+    ]
 
     assert.deepStrictEqual(
       telemetry.job.emit.args.filter(event => event[0] === 'cancelled'),
       [
-        ['cancelled', expectedData],
-        ['cancelled', expectedData],
+        ['cancelled', expectedData[0]],
+        ['cancelled', expectedData[1]],
       ]
     )
   })
 
   it('Should emit "delivered" events', function() {
-    const expectedData = {
-      autoParked:      true,
-      isSpecial:       true,
-      cargo:           'foo',
-      market:          'bar',
-      cargoDamage:     2000,
-      deliveryTime:    2001,
-      distance:        2002,
-      earnedXP:        2003,
-      finishingTime:   2004,
-      plannedDistance: 3002,
-      revenue:         2005,
-      startingTime:    2006,
-      destination:     {foo: 'bar'},
-      source:          {bar: 'baz'},
-    }
+    const expectedData = [
+      {
+        autoParked:      true,
+        isSpecial:       false,
+        cargo:           'oof',
+        market:          'rab',
+        cargoDamage:     4000,
+        deliveryTime:    5000,
+        distance:        6000,
+        earnedXP:        7000,
+        finishingTime:   8000,
+        plannedDistance: 4101,
+        revenue:         9000,
+        startingTime:    1100,
+        destination:     {foo: 'zab'},
+        source:          {bar: 'xuq'},
+      },
+      {
+        autoParked:      true,
+        isSpecial:       false,
+        cargo:           'oof',
+        market:          'rab',
+        cargoDamage:     4000,
+        deliveryTime:    5000,
+        distance:        6000,
+        earnedXP:        7000,
+        finishingTime:   8000,
+        plannedDistance: 4103,
+        revenue:         9000,
+        startingTime:    1100,
+        destination:     {foo: 'zab'},
+        source:          {bar: 'xuq'},
+      }
+    ]
 
     assert.deepStrictEqual(
       telemetry.job.emit.args.filter(event => event[0] === 'delivered'),
       [
-        ['delivered', expectedData],
-        ['delivered', expectedData],
+        ['delivered', expectedData[0]],
+        ['delivered', expectedData[1]],
       ]
     )
   })
